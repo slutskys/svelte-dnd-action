59a2eb2b6137fde29406be8d695e42a24898ca7c
Quentin Fox <quentinbennettfox@gmail.com>
2022-02-17 16:08:58 -0800

    add typescript eslint setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,10 @@
 module.exports = {
+    parser: "@typescript-eslint/parser",
     env: {
         browser: true,
         es2021: true
     },
-    extends: "eslint:recommended",
+    extends: ["eslint:recommended", "plugin:@typescript-eslint/eslint-recommended", "plugin:@typescript-eslint/recommended"],
     parserOptions: {
         ecmaVersion: 12,
         sourceType: "module"
