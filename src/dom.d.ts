9c78c50fa156848eca7cf79ade5d643995da23e6
Quentin Fox <quentinbennettfox@gmail.com>
2022-07-10 23:27:55 -0700

    add global definition for events emitted on HTMLElement and Window

---
+++ src/dom.d.ts
@@ -0,0 +1,13 @@
+import { DraggedEnteredEvent, DraggedLeftDocumentEvent, DraggedLeftEvent, DraggedOverIndexEvent } from './internalTypes';
+
+declare global {
+  interface HTMLElementEventMap {
+    'draggedEntered': DraggedEnteredEvent;
+    'draggedLeft': DraggedLeftEvent;
+    'draggedOverIndex': DraggedOverIndexEvent;
+  }
+
+  interface WindowEventMap {
+    'draggedLeftDocument': DraggedLeftDocumentEvent;
+  }
+}
