1b79f54ca8eff3daf7f07636f98fde20cc3ecb24
Quentin Fox <quentinbennettfox@gmail.com>
2022-07-12 00:38:24 -0700

    convert svelteNodeClone to ts

---
+++ src/helpers/svelteNodeClone.ts
@@ -0,0 +1,42 @@
+/**
+ * Fixes svelte issue when cloning node containing (or being) <select> which will loose it's value.
+ * Since svelte manages select value internally.
+ * @see https://github.com/sveltejs/svelte/issues/6717
+ * @see https://github.com/isaacHagoel/svelte-dnd-action/issues/306
+ *
+ * @param {HTMLElement} el
+ * @returns
+ */
+export function svelteNodeClone(el: HTMLElement) {
+    const cloned = el.cloneNode(true);
+
+    if (!(cloned instanceof HTMLElement)) {
+        throw new Error("cloned HTMLElement did not return new HTMLElement");
+    }
+
+    if (el instanceof HTMLSelectElement && cloned instanceof HTMLSelectElement) {
+        const values = [];
+        const elIsSelect = el.tagName === "SELECT";
+        const selects = elIsSelect ? [el] : [...el.querySelectorAll("select")];
+
+        for (const select of selects) {
+            values.push(select.value);
+        }
+
+        if (selects.length <= 0) {
+            return cloned;
+        }
+
+        const clonedSelects = elIsSelect ? [cloned] : [...cloned.querySelectorAll("select")];
+        for (let i = 0; i < clonedSelects.length; i++) {
+            const select = clonedSelects[i];
+            const value = values[i];
+            const optionEl = select.querySelector(`option[value="${value}"`);
+            if (optionEl) {
+                optionEl.setAttribute("selected", "true");
+            }
+        }
+    }
+
+    return cloned;
+}
